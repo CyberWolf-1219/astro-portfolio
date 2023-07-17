124a18eb6472e285e59661a7f95afd6b8002b162
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-07-17 20:47:38 +0530

    created terminal like ui

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { USER_SKILLS } from '../../data/skills';
 
 function SkillsSection() {
   return (
     <section>
       <div>
-        <h1>Skills Section</h1>
+        <h2>@Skills Section&gt;_</h2>
+        <div className={'w-full h-fit py-[5px] bg-pallet-primary-light'}>
+          <code className={'w-full h-fit  text-pallet-accent'}>
+            brain@dev&gt; ls user-skills
+            <ul className={'w-full h-fit'}>
+              <li>---&nbsp;&nbsp;&nbsp;&nbsp;.</li>
+              <li>---&nbsp;&nbsp;&nbsp;&nbsp;..</li>
+              {USER_SKILLS.map((skill) => {
+                return (
+                  <li key={Math.random()}>
+                    ---&nbsp;&nbsp;&nbsp;&nbsp;{skill}
+                  </li>
+                );
+              })}
+            </ul>
+          </code>
+        </div>
       </div>
     </section>
   );

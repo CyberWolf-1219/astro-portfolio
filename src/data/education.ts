e9571c108e626e1beb51ca8b47c78cb0b573a36c
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-07-17 20:58:08 +0530

    added static data files

---
+++ src/data/education.ts
@@ -0,0 +1,95 @@
+export const EDUCAION_MILESTONES = [
+  {
+    institute: 'Udemy',
+    courses: [
+      {
+        course: 'React.js/Redux',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Node.js/Express.js',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Next.js',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Git/GitHub',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Javascript',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Typescript',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Javascript Unit Testing',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Java / OOP',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'UML',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'Docker',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+      {
+        course: 'CI/CD (GitHub Actions)',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+    ],
+  },
+  {
+    institute: 'edX',
+    courses: [
+      {
+        course: 'CS50',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+    ],
+  },
+  {
+    institute: 'Coursera',
+    courses: [
+      {
+        course: 'CS50',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe temporibus, esse facere laborum, aspernatur aliquid sunt quo dolores quasi incidunt architecto illum eos? Magni voluptates expedita repellat sint. Quam, impedit!',
+        link: '',
+      },
+    ],
+  },
+];

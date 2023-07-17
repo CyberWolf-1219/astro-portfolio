3fbcc0edf019db2b20b50f0dd210a15f63296a3c
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-07-17 20:44:23 +0530

    added dummy details

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 function HeroSection() {
   return (
-    <section>
+    <section id={'about'}>
       <div>
-        <h1>Hero Section</h1>
+        <h1
+          className={
+            'text-center text-pallet-light border-y-[2px] border-pallet-light '
+          }>
+          FULL-STACK DEVELOPER
+        </h1>
+        <h2 className={'text-center !text-xl'}>SELF-TAUGHT FROM SCRATCH</h2>
       </div>
     </section>
   );

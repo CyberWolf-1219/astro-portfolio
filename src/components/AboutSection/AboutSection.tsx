37d2c30c063624aef39ca29aeabbd7b5e422101b
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-07-17 20:45:25 +0530

    added ui elements

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -3,8 +3,31 @@ import React from 'react';
 function AboutSection() {
   return (
     <section>
-      <div>
-        <h1>About Section</h1>
+      <div className={'w-full h-fit'}>
+        <h2>@About Section&gt;_</h2>
+        <div>
+          <img
+            src=''
+            alt=''
+            className={
+              'aspect-[4/5] w-full max-w-[500px] h-auto bg-pallet-accent'
+            }
+          />
+          <p className={'text-justify'}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolore
+            aliquam omnis quae illo sed, obcaecati quos autem pariatur suscipit
+            deleniti assumenda eligendi unde incidunt. Adipisci commodi
+            delectus, ratione doloremque temporibus sit dignissimos ab, veniam
+            et voluptates provident? Eaque laudantium, natus nesciunt quod non
+            tempore nisi culpa molestiae sit modi aspernatur ullam a, nihil
+            animi? Iusto aperiam quibusdam perspiciatis velit laboriosam
+            explicabo expedita impedit, aliquam maxime facilis laborum ipsum
+            libero cumque ad adipisci officiis, repellat beatae deserunt magnam
+            nihil sapiente animi! Ipsum, debitis aspernatur voluptates delectus
+            nam, pariatur laudantium fugiat, dicta illum porro necessitatibus
+            reiciendis repellat nemo expedita molestias soluta.
+          </p>
+        </div>
       </div>
     </section>
   );

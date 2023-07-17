c964447d503d406e56ad7e8aace0bb92ff4d3a7e
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-07-17 20:54:46 +0530

    created logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Logo() {
-  return <h1>&lt;LAHIRUA.RAJAKARUNA/&gt;</h1>;
+  return (
+    <h1
+      className={
+        'w-fit h-fit !mx-auto !my-[0] text-center !text-2xl text-pallet-accent !font-semibold border-y-[2px] border-pallet-accent'
+      }>
+      &lt;LAHIRU A. RAJAKARUNA/&gt;
+    </h1>
+  );
 }
 
 export default Logo;

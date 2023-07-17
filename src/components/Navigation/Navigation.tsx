b6dc03e70b09ef0ad803f262d834426d0299e9e7
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-07-17 20:56:39 +0530

    created navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,13 +4,27 @@ import { TOP_NAV_LINKS } from '../../data/navigation';
 
 function Navigation() {
   return (
-    <nav>
-      <div>
+    <nav
+      className={
+        'px-[5%] py-[2.5%] bg-pallet-primary-light shadow-md shadow-pallet-primary  text-pallet-light'
+      }>
+      <div
+        className={
+          'w-full max-w-[1280px] h-fit mx-auto flex flex-col items-center justify-start gap-[15px]'
+        }>
         <Logo />
         <ul
           className={
             'w-fit h-fit flex flex-row items-center justify-between gap-[10px]'
-          }></ul>
+          }>
+          {TOP_NAV_LINKS.map(({ text, link }, index) => {
+            return (
+              <li key={`nav_link_${index}`}>
+                <a href={link}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );

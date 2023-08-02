fbe2b1cdb844b60f1e88ef0af82a067a24da5c50
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-08-02 07:04:34 +0530

    added entrance animation

--- src/components/Services/ServiceCard/ServiceCard.tsx
+++ src/components/Services/ServiceCard/ServiceCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { Service } from '../../../types';
 
+import css from './style.module.css';
+
 interface Props {
   service: Service;
 }
@@ -8,9 +10,7 @@ interface Props {
 function ServiceCard({ service }: Props) {
   return (
     <div
-      className={
-        'shrink-0 w-full h-fit px-[20px] py-[30px] bg-pallet-primary-light shadow-[0px_0px_10px_2px] shadow-transparent border-[1px] border-pallet-accent hover:shadow-pallet-accent/30 duration-[300ms]'
-      }>
+      className={`${css.card} shrink-0 w-full h-fit px-[20px] py-[30px] bg-pallet-primary-light shadow-[0px_0px_10px_2px] shadow-transparent border-[1px] border-pallet-accent hover:shadow-pallet-accent/30 duration-[300ms]`}>
       <table className={'w-full h-fit  text-center'}>
         <thead className={'text-xl'}>
           <tr>
@@ -23,20 +23,22 @@ function ServiceCard({ service }: Props) {
               <img
                 src={service.serviceImage.imageLink}
                 alt={service.serviceImage.imageCaption}
-                className={'aspect-[1/1] w-full h-auto'}
+                className={'aspect-[1/1] w-full max-w-[150px] h-auto mx-auto'}
               />
             </td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
-            <a
-              href='mailto:bl4ckm0nk3yb4n4n4@gmail.com'
-              className={
-                'w-full h-fit px-[0.75em] py-[0.5em] bg-pallet-accent'
-              }>
-              EMAIL ME
-            </a>
+            <td>
+              <a
+                href='mailto:bl4ckm0nk3yb4n4n4@gmail.com'
+                className={
+                  'w-full h-fit px-[0.75em] py-[0.5em] bg-pallet-accent'
+                }>
+                EMAIL ME
+              </a>
+            </td>
           </tr>
         </tfoot>
       </table>

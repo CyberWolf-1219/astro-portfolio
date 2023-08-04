b13f695bab27c02e630377cdd2a5b0b726d52cc0
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-08-04 12:08:49 +0530

    added bg and made the table a flex

--- src/components/Services/ServiceCard/ServiceCard.tsx
+++ src/components/Services/ServiceCard/ServiceCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Service } from '../../../types';
 
 import css from './style.module.css';
+import { CONTACTS } from '../../../data/contacts';
 
 interface Props {
   service: Service;
@@ -10,14 +11,17 @@ interface Props {
 function ServiceCard({ service }: Props) {
   return (
     <div
-      className={`${css.card} shrink-0 w-full h-fit px-[20px] py-[30px] bg-pallet-primary-light shadow-[0px_0px_10px_2px] shadow-transparent border-[1px] border-pallet-accent hover:shadow-pallet-accent/30 duration-[300ms]`}>
-      <table className={'w-full h-fit  text-center'}>
+      className={`${css.card} relative shrink-0 aspect-[4/5] w-full h-auto p-[15px] bg-pallet-primary-light shadow-[0px_0px_10px_2px] shadow-transparent border-[1px] border-pallet-accent hover:shadow-pallet-accent/30 duration-[300ms] overflow-hidden`}>
+      <table
+        className={
+          'w-full h-full text-center flex flex-col items-center justify-between gap-[10px]'
+        }>
         <thead className={'text-xl'}>
           <tr>
             <th>{service.serviceTitle}</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className={''}>
           <tr>
             <td>
               <img
@@ -28,16 +32,27 @@ function ServiceCard({ service }: Props) {
             </td>
           </tr>
         </tbody>
-        <tfoot>
+        <tfoot className={''}>
           <tr>
             <td>
-              <a
-                href='mailto:bl4ckm0nk3yb4n4n4@gmail.com'
+              <ul
                 className={
-                  'w-full h-fit px-[0.75em] py-[0.5em] bg-pallet-accent'
+                  'w-full h-fit py-[15px] flex flex-row items-center justify-center gap-[15px]'
                 }>
-                EMAIL ME
-              </a>
+                {CONTACTS.map((contact) => {
+                  return (
+                    <li key={`contact_${Math.random()}`}>
+                      <a
+                        href={contact.contact}
+                        className={
+                          'w-full h-fit px-[0.75em] py-[0.5em] bg-pallet-accent'
+                        }>
+                        {contact.type}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
             </td>
           </tr>
         </tfoot>

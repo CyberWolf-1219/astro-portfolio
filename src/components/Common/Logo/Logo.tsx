7ba459e90671779a1b49e80f3d5f7e74efbb49c5
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-08-07 14:35:09 +0530

    added production logo

--- src/components/Common/Logo/Logo.tsx
+++ src/components/Common/Logo/Logo.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 function Logo() {
   return (
-    <h1
-      className={
-        'w-fit h-fit !mx-auto !my-[0] text-center !text-2xl text-pallet-accent !font-semibold border-y-[2px] border-pallet-accent'
-      }>
-      &lt;LAHIRU A. RAJAKARUNA/&gt;
-    </h1>
+    <img
+      src='/logo-thin.png'
+      alt='lahiru anjana rajakaruna personal logo'
+      className={'w-full max-w-[300px] h-auto object-center'}
+    />
   );
 }
 

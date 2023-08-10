b560ef8f96c0cb6279abcdbf728a45237cbf318a
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-08-10 13:10:32 +0530

    replaced skills section asset

--- src/components/Home/SkillsSection/SkillsSection.tsx
+++ src/components/Home/SkillsSection/SkillsSection.tsx
@@ -11,14 +11,9 @@ function SkillsSection() {
             'w-full h-fit py-[5px] flex flex-col items-center justify-start gap-[50px]'
           }>
           <img
-            src='/software_development.svg'
+            src='/dev_skills_graph.svg'
             alt='lahiru anjana software development skills svg bubble graph'
-            className={'aspect-[16/9] w-full h-auto'}
-          />
-          <img
-            src='/web_development.svg'
-            alt='lahiru anjana web development skills svg bubble graph'
-            className={'aspect-[16/9] w-full h-auto'}
+            className={'aspect-[9/16] w-full h-auto'}
           />
         </div>
       </div>

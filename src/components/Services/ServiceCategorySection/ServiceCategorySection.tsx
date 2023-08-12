c95ffdf8abb9800a05564ffed870dfde8854df3e
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-08-12 17:10:31 +0530

    added a background animation

--- src/components/Services/ServiceCategorySection/ServiceCategorySection.tsx
+++ src/components/Services/ServiceCategorySection/ServiceCategorySection.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import css from './style.module.css';
+
 import type { ServiceCategory } from '../../../types';
 import ServiceCard from '../ServiceCard/ServiceCard';
 
@@ -20,13 +22,24 @@ function ServiceCategorySection({ serviceCategory }: Props) {
           }>
           {serviceCategory.categoryName}
         </h2>
-        <img
-          src={serviceCategory.categoryImage.imageLink}
-          alt={serviceCategory.categoryImage.imageCaption}
+        <div
           className={
-            'md:absolute z-[1] top-[50%] md:left-[50%] left-[5%] md:translate-y-[-50%] md:translate-x-[-50%] aspect-[1/1] w-full md:max-w-[300px] lg:max-w-[500px] h-auto md:blur-[.5px]'
-          }
-        />
+            'relative aspect-[1/1] w-full h-auto p-[5rem] bg-pallet-primary-light rounded-full overflow-hidden'
+          }>
+          <div
+            className={`${css.style_element_1} absolute z-[1] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] aspect-[1/1] w-[60%] h-auto rounded-full border-x-[5px]`}></div>
+          <div
+            className={`${css.style_element_2} absolute z-[1] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] aspect-[1/1] w-[70%] h-auto rounded-full border-y-[5px]`}></div>
+          <div className={'relative z-[3] aspect-[1/1] w-full h-auto'}>
+            <img
+              src={serviceCategory.categoryImage.imageLink}
+              alt={serviceCategory.categoryImage.imageCaption}
+              className={
+                'md:absolute z-[1] top-[50%] md:left-[50%] left-[5%] md:translate-y-[-50%] md:translate-x-[-50%] aspect-[1/1] w-full md:max-w-[300px] lg:max-w-[500px] h-auto md:blur-[.5px]'
+              }
+            />
+          </div>
+        </div>
         <ul
           className={
             'md:absolute z-[3] bottom-[5%] left-[5%] lg:left-[25%] right-[5%] w-[100%] md:w-auto md:pb-[10px] h-fit md:p-[25px] mt-[50px] flex flex-col md:flex-row items-center justify-start gap-[10px] snap-both snap-mandatory overflow-auto'

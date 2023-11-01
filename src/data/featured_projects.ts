e30574193b658d9421016d7d0f8996808774ff2f
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-01 14:14:03 +0530

    added new projects

--- src/data/featured_projects.ts
+++ src/data/featured_projects.ts
@@ -1,3 +1,36 @@
 import type { FeaturedProject } from '../types';
 
-export const FEATURED_PROJECTS: FeaturedProject[] = [];
+export const FEATURED_PROJECTS: FeaturedProject[] = [
+    {
+        projectTitle: 'Resume Builder Application',
+        projectLink: 'https://junior-resume-builder.netlify.app/',
+        projectImage: {
+            imageCaption: 'React Typescript Resume Builder Application',
+            imageLink: '/project_images/resume_builder.png'
+        }
+    },
+    {
+        projectTitle: 'Wether Dashboard',
+        projectLink: 'https://weather-music-player.netlify.app/',
+        projectImage: {
+            imageCaption: 'React Typescript Wether Dashboard',
+            imageLink: '/project_images/wether_dashboard.png'
+        }
+    },
+    {
+        projectTitle: 'Past Exam Paper Database',
+        projectLink: 'https://pastpaperdatabase.netlify.app/',
+        projectImage: {
+            imageCaption: 'A database of past exam papers for sri lankan students',
+            imageLink: '/project_images/ppdb.png'
+        }
+    },
+    {
+        projectTitle: 'Car Rental Service Laning Page',
+        projectLink: 'https://pastpaperdatabase.netlify.app/',
+        projectImage: {
+            imageCaption: 'landing page of a car rental service built with react typescript tailwind gsap',
+            imageLink: '/project_images/crslp.png'
+        }
+    },
+];

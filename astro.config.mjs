201658d5e74e533c8858ade5d54e50699256b525
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-01 13:33:31 +0530

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
   site: 'https://lahiru-a-r.netlify.app',
   integrations: [react(), tailwind(), sitemap()],
+  adapter: netlify(),
 });

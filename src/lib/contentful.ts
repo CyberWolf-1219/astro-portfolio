4151a4ec4dfb1b39355a257f9c22e876fde6b354
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-01 22:27:39 +0530

    created contentful client class

---
+++ src/lib/contentful.ts
@@ -0,0 +1,53 @@
+import { ContentfulClientApi, createClient } from 'contentful';
+import type { BlogPostSkeleton } from '../types';
+
+export class Contentful {
+  private client: ContentfulClientApi<undefined> | null = null;
+
+  constructor() {
+    this.client = createClient({
+      space: import.meta.env.PUBLIC_CONTENTFUL_SPACE_ID,
+      accessToken: import.meta.env.PUBLIC_CONTENTFUL_DELIVERY_TOKEN,
+    });
+  }
+
+  public async entryTypes() {
+    try {
+      const result = await this.client?.getContentTypes();
+      return result
+    } catch (e) {
+      console.log('[-] CONTENTFUL ERROR');
+      console.log(e);
+    }
+  }
+
+  public async entryType(entryTypeID: string) {
+    try {
+      const result = await this.client?.getContentType(entryTypeID);
+      return result
+    } catch (e) {
+      console.log('[-] CONTENTFUL ERROR');
+      console.log(e);
+    }
+  }
+
+  public async getEntries(type?: string) {
+    try {
+      const result = await this.client?.getEntries<BlogPostSkeleton>({ query: type });
+      return result;
+    } catch (e) {
+      console.log('[-] CONTENTFUL ERROR');
+      console.log(e);
+    }
+  }
+
+  public async getEntry(type: string, id: string) {
+    try {
+      const result = await this.client?.getEntries<BlogPostSkeleton>();
+      return result;
+    } catch (e) {
+      console.log('[-] CONTENTFUL ERROR');
+      console.log(e);
+    }
+  }
+}

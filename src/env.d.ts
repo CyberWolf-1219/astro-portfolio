27011a135e33ce1132f93bd7a1a0ea721195e3cd
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-01 22:26:53 +0530

    added contentful env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_CONTENTFUL_SPACE_ID: string;
+  readonly PUBLIC_CONTENTFUL_DELIVERY_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

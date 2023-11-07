855bde3c338ba451896c0c9fc1b1735d73c0805f
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 10:33:07 +0530

    updated project data

--- src/data/featured_projects.ts
+++ src/data/featured_projects.ts
@@ -7,7 +7,8 @@ export const FEATURED_PROJECTS: FeaturedProject[] = [
         projectImage: {
             imageCaption: 'React Typescript Resume Builder Application',
             imageLink: '/project_images/resume_builder.png'
-        }
+        },
+        projectDesc: "Multi-step resume builder application built with react, typescript, and tailwind. Heaviliy uses contextAPI, timeout, intervals. Reduced re-renders for optimization. Used ContextAPI, timeouts and setIntervals for real-time tempate updates."
     },
     {
         projectTitle: 'Wether Dashboard',
@@ -15,7 +16,8 @@ export const FEATURED_PROJECTS: FeaturedProject[] = [
         projectImage: {
             imageCaption: 'React Typescript Wether Dashboard',
             imageLink: '/project_images/wether_dashboard.png'
-        }
+        },
+        projectDesc: "Wether dashboard developed with react and typescript. Fetches wether data from a third-party api. Features a fully functional music-player developed using WebAudio API and CanvasAPI for freaquency band visualization."
     },
     {
         projectTitle: 'Past Exam Paper Database',
@@ -23,14 +25,16 @@ export const FEATURED_PROJECTS: FeaturedProject[] = [
         projectImage: {
             imageCaption: 'A database of past exam papers for sri lankan students',
             imageLink: '/project_images/ppdb.png'
-        }
+        },
+        projectDesc: "Simple website developed with astro.js and firebase to store past exam papers for Sri Lankan school students."
     },
     {
         projectTitle: 'Car Rental Service Laning Page',
-        projectLink: 'https://pastpaperdatabase.netlify.app/',
+        projectLink: 'https://crslp.netlify.app/',
         projectImage: {
             imageCaption: 'landing page of a car rental service built with react typescript tailwind gsap',
             imageLink: '/project_images/crslp.png'
-        }
+        },
+        projectDesc: "Modern landing page developed with React, Tailwind, GSAP."
     },
 ];

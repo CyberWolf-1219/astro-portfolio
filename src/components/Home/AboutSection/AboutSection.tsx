26a9a2c1a8e0ba9ceaa07bb5ea93fabb5cf8577b
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 06:44:20 +0530

    changed heading

--- src/components/Home/AboutSection/AboutSection.tsx
+++ src/components/Home/AboutSection/AboutSection.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ABOUT_ME } from '../../../data/about_me';
 
+// TODO: UPDATE THE COPY
+
 function AboutSection() {
   return (
     <section className={'bg-pallet-primary-light'}>
       <div className={'w-full h-fit '}>
-        <h2>{ABOUT_ME.heading}</h2>
+        <h2>@About Me&gt;_</h2>
         <div
           className={
             'w-full h-fit flex flex-col md:block items-start justify-start'
@@ -17,6 +19,7 @@ function AboutSection() {
               'shrink-0 aspect-[4/5] md:aspect-[1/1] xl:aspect-[1/1] w-full max-w-[400px] h-auto md:mr-[10px] md:float-left bg-pallet-accent object-cover'
             }
           />
+          <h3 className={'!text-2xl'}>{ABOUT_ME.title}</h3>
           {ABOUT_ME.text.split('\n').map((para, index) => {
             return (
               <p

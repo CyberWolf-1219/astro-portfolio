d454c16b4a5d981338b21f63cfd2fa2feae2c40b
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 06:44:00 +0530

    changed layout

--- src/components/Blog/BlogPostSearchResultCard/BlogPostSearchResultCard.tsx
+++ src/components/Blog/BlogPostSearchResultCard/BlogPostSearchResultCard.tsx
@@ -16,29 +16,36 @@ function BlogPostSearchResultCard({ postIndex, post, animationDelay }: Props) {
   return (
     <article
       style={{ animationDelay: `${animationDelay}ms` }}
-      className={`BlogPostSearchResultCard aspect-[9/16] w-full h-auto p-[10px] flex flex-col items-center justify-start gap-[10px] bg-pallet-accent/50 border-[2px] border-pallet-accent overflow-hidden delay-[${animationDelay}ms]`}>
-      <h1
-        className={'w-full h-fit !my-[0px] !text-2xl !font-semibold underline'}>
-        {post.fields.title as string}
-      </h1>
-      <p className={'w-full h-fit text-left'}>
-        <small>{post.fields.createdDate as string}</small>
-      </p>
-      <img
-        src={`https:${(post.fields.heroImage as Asset).fields.file?.url}`}
-        alt=''
-        className={'shrink-0 aspect-[4/3] w-full h-auto bg-pallet-primary'}
-      />
-      <p className={'w-full h-fit text-justify line-clamp-[9]'}>
-        {documentToReactComponents(post.fields.content)}
-      </p>
-      <p className={'w-full h-fit mt-auto text-left'}>
-        <a
-          href={`/blog/${post.sys.id}`}
-          className={'font-bold'}>
-          Read Post &gt;&gt;
-        </a>
-      </p>
+      className={`BlogPostSearchResultCard aspect-[10/3] w-full h-auto p-[1rem] flex flex-col md:flex-row items-center justify-start gap-[1rem] bg-pallet-accent/50 border-[2px] border-pallet-accent overflow-hidden delay-[${animationDelay}ms]`}>
+      <div className={'aspect-[4/3] md:aspect-[1/1] w-auto h-full'}>
+        <img
+          src={`https:${(post.fields.heroImage as Asset).fields.file?.url}`}
+          alt=''
+          className={
+            'shrink-0 aspect-[4/3] md:aspect-[1/1] w-full h-auto bg-pallet-primary'
+          }
+        />
+      </div>
+      <div className={'w-full h-full'}>
+        <h1
+          className={
+            'w-full h-fit !m-[0px] !text-2xl !font-semibold underline'
+          }>
+          {post.fields.title as string}
+        </h1>
+        <p className={'w-full h-fit text-left'}>
+          <small>{post.fields.createdDate as string}</small>
+        </p>
+        {documentToReactComponents(post.fields.content.content[0])}
+        <p>{post.fields.content.content[1]!.content[0].value}</p>
+        <p className={'w-full h-fit mt-auto text-left'}>
+          <a
+            href={`/blog/${post.sys.id}`}
+            className={'font-bold'}>
+            Read Post &gt;&gt;
+          </a>
+        </p>
+      </div>
     </article>
   );
 }

78d8861b327b95bea35d016924491741e7b5fc16
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-11-07 06:46:45 +0530

    removed functionality

--- src/components/Blog/BlogPostLayout/BlogPostLayout.tsx
+++ src/components/Blog/BlogPostLayout/BlogPostLayout.tsx
@@ -1,48 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import BlogPostHeroSection from '../BlogPostHeroSection/BlogPostHeroSection';
-import BlogPostSubSection from '../BlogPostSubSection/BlogPostSubSection';
-import axios from 'axios';
-import { BLOG_POSTS } from '../../../data/blogposts';
-import type { BlogPost } from '../../../types';
+import React from 'react';
 
 interface Props {
-  postID: number;
+  post: unknown;
 }
-
-function BlogPostLayout({ postID }: Props) {
-  const [blogPost, setBlogPost] = useState<BlogPost>(BLOG_POSTS[0]);
-
-  useEffect(() => {
-    async function fetchPostSections() {
-      const RESPONSE = await axios.get('api/blog', { params: { post_id: 1 } });
-      console.group('BLOGPOST SECTION FETCH');
-      console.log(RESPONSE);
-      console.groupEnd();
-    }
-    console.group('BLOG POST');
-    console.log(blogPost);
-    console.groupEnd();
-  }, [blogPost]);
-
-  return (
-    <>
-      {blogPost.sections.map((section, index) => {
-        if (section.type == 'hero') {
-          return (
-            <BlogPostHeroSection
-              heading={section.heading}
-              image={section.image!}
-              summaryText={section.text}
-              createdDate={blogPost.createdDate}
-              author={blogPost.author}
-            />
-          );
-        } else {
-          return <BlogPostSubSection data={section} />;
-        }
-      })}
-    </>
-  );
+function BlogPostLayout({ post }: Props) {
+  return <article>{/* TODO: RENDER BLOG POST */}</article>;
 }
 
 export default BlogPostLayout;
